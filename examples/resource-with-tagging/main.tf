2a0f3d33c69e4f6f35a8776f32a63750b7d2a8cd
Kurtis Mash <24705116+kurtismash@users.noreply.github.com>
2025-10-13 11:02:24 +0100

    Add example for creating OUs with tagging

---
+++ examples/resource-with-tagging/main.tf
@@ -0,0 +1,10 @@
+module "ous" {
+  source = "../../"
+  # source  = "nationalarchives/organizations-ous-by-path/aws"
+  # version = "1.1.0"
+
+  name_path_delimiter    = " / "
+  organization_structure = local.org_structure
+  static_tags_key        = "@tags"
+  cascading_tags_key     = "Vtags"
+}
