6fbd86b9b88fb35d14b1aa43f1510cc832297528
Kurtis Mash <24705116+kurtismash@users.noreply.github.com>
2025-10-15 09:07:42 +0100

    Add tags output to data example

--- examples/data/outputs.tf
+++ examples/data/outputs.tf
@@ -1,9 +1,14 @@
-output "backup_ou" {
+output "backup_ou_id" {
   description = "Backup OU ID."
   value       = module.ous.by_name_path["Backup"].id
 }
 
-output "app_prod_ou" {
+output "app_prod_ou_id" {
   description = "Application Production OU ID."
   value       = module.ous.by_name_path["Workloads / Application / Production"].id
+}
+
+output "serverless_ou_tags" {
+  description = "Map of tags associated with the Serverless OU."
+  value       = module.ous.by_name_path["Workloads / Serverless"].tags
 }
